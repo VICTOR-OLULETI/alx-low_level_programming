e52056712a862301956c603bb875a5906f44a59a
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-08 12:43:47 -0700

    changes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	char letters;
+	for (letters ='a'; letters< 'z';letters++)
+		putchar(letters);
+	putchar("\n");
+	return (0);
+}
