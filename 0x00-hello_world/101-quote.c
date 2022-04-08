61836f83368b4825a37e92ac8d11102c8d7e76bb
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-08 01:45:38 -0700

    using write to print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 /**
  * main - Entry point
  *
@@ -8,6 +9,9 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
+	write(1,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	       	59);
+
 	return (1);
 } 
