7c67802784f14538d9c4041a9551a69f4a12f5cd
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-08 18:36:16 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,8 +14,8 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 < 98; num2++)
 		{
-			putchar((num / 10) + '0');
-			putchar((num % 10) + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
 			putchar(' ');
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
