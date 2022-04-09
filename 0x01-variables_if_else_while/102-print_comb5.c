5e8e8ab36026e3320256ad371bf5175019418fa3
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-08 18:33:28 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print all combinations of two two digit numbers
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 98; num2++)
+		{
+			putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
