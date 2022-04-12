eccf2793f319466191241e31c0051d40c24e2bf1
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-12 12:31:32 -0700

    prints 1- times the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+
+int print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	i = 0;
+	if (i<10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	return (0);
+}
