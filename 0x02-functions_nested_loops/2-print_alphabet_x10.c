c3022976e28505a384eadf598d7896bd8524acc9
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-12 12:41:31 -0700

    prints 1- times the alphabet in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,12 +12,10 @@ void print_alphabet_x10(void)
 	int i;
 
 	i = 0;
-	if (i < 10)
+	while (i < 10)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
-		{
 			_putchar(letter);
-		}
 		i++;
 		_putchar('\n');
 	}
