dc6f39b8334443404e3d15a5e44bc10110836ad8
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-12 15:57:28 -0700

    function that prints out absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,15 +11,15 @@ int _abs(int n)
 	if (n < 0)
 	{
 		n *= -1;
-		_putchar((n % 10) + '0');
+		return (n);
 	}
 	else if (n > 0)
 	{
-		_putchar(n);
+		return (n);
 	}
 	else
 	{
-		_putchar('0');
+		return (0);
 	}
-	return (0);
+	
 }
