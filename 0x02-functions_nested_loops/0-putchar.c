6a2d213ae2186d655f02698708a6a33d3104085b
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-12 10:28:17 -0700

    print _putchar followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,17 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('a');
+	_putchar('.');
+	_putchar('c');
+	_putchar('\n');
 
 	return (0);
 }
