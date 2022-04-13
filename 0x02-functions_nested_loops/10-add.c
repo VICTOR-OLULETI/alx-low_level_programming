91e4845ade2a53a027aaac81bf683c73900a0a96
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-12 19:19:29 -0700

    add two integers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  * add - function that sums up two integers and returns the result
  * @a: a is the first integer argument
  * @b : b is the second integer argument
- * Result: Always 0.
+ * Return: Returns the sum of the two integers.
  */
 
 int add(int a, int b)
