be9eccdd22af8656d062858fa4caecfda809fd33
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-12 19:17:30 -0700

    add two integers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *add - function that sums up two integers and returns the result
- *@a: a is the first integer argument
- *@b : b is the second integer argument
+ * add - function that sums up two integers and returns the result
+ * @a: a is the first integer argument
+ * @b : b is the second integer argument
  * Result: Always 0.
  */
 
