36c4e2457eec42b5edd4769125868892bf3a5183
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-12 20:44:04 -0700

    prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,12 +13,12 @@ void print_times_table(int n)
 	int i;
 	int j;
 	int result;
-	
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			putchar('0');
+			_putchar('0');
 			for (j = 1; j <= n; j++)
 			{
 				result = i * j;
