2ece810ba8c9d97e41393515939b676d01adfad6
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-14 13:29:42 -0700

    checks whether uppercase or not

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,12 @@
 /**
  * _isupper - checks for uppercase character
  *
- * Return: Always 0.
+ * Return: Always 0 or 1.
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
