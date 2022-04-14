cc171d8c20bc0de812e023266e308b9697cb361f
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-13 21:32:16 -0700

    prints the first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,13 +11,11 @@
 int main(void)
 {
 	int i;
-	unsigned long fib1, fib2, sum;
+	unsigned long fib1, fib2, sum, half1, half2;
 	unsigned long fib1_half1, fib2_half1, fib1_half2, fib2_half2;
-	unsigned long half1, half2;
 
 	fib1 = 0;
 	fib2 = 1;
-
 	for (i = 0; i < 92; i++)
 	{
 		sum = fib1 + fib2;
@@ -25,28 +23,22 @@ int main(void)
 		fib1 = fib2;
 		fib2 = sum;
 	}
-
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
-
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
-
 	for (i = 93; i < 99; i++)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 10000000000;
 		}
-
 		printf("%lu%lu", half1, half2);
 		if (i != 98)
 			printf(", ");
-
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
