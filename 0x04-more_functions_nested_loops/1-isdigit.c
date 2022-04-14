af6cf82c69562be133f265a95cf06e8686fd64bc
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-14 15:02:58 -0700

    checks if a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ *
+ * @c: c is the digit to be checked
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
