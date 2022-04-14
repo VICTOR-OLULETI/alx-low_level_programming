c95a5c5db6b12f2d0be7b47e8de9e73c1615b804
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-14 10:34:26 -0700

    converts a dat to the day of year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -5,7 +5,7 @@
  * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
  * @month: month in number format
- * @day: day of onth
+ * @day: day of month
  * @year: year
  * Return: void
  */
@@ -27,8 +27,8 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n"
-			       , month, day - 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n",
+			       month, day - 31, year);
 		}
 		else
 		{
