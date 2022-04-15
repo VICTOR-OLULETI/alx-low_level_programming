fe9bd54adb8d8b7712a1b8db7f034445c9746ea1
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-15 07:03:29 -0700

    print largest prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	unsigned int long num = 612852475143;
+	unsigned int long count = sqrt(num);
+
+	while (1)
+	{
+		if (num % count == 0)
+		{
+			printf("%lu\n", num / count);
+			break;
+		}
+		count--;
+	}
+	return (0);
+}
