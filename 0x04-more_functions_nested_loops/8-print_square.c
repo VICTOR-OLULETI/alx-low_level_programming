cee9fe78809aba74103764e4b805199f4a9368d3
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-14 19:46:28 -0700

    draw a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,18 +11,21 @@ void print_square(int size)
 	int i, j;
 
 	i = 0;
-	while (i < size && size > 0)
+	if (size > 0)
 	{
-		j = 0;
-		while (j < n)
-		{
-			_putchar('#');
-			j++;
-		}
 
-		_putchar('\n');
-		i++;
+		while (i < size)
+		{
+			j = 0;
+			while (j < n)
+			{
+				_putchar('#');
+				j++;
+			}
+
+			_putchar('\n');
+			i++;
+		}
 	}
-	if ( i == 0)
-		_putchar('\n');
+ 	_putchar('\n');
 }
