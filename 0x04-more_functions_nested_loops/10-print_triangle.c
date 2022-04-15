74bcc2ddbcb5a703e15650fd60951179b1414cab
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-15 05:14:59 -0700

    draw a triangel

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followoed by a new line.
+ * @size: determines the size of the triangle
+ * Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+	int i = 1;
+	int j;
+
+	if ( size > 0)
+	{
+		while ( i <= size)
+		{
+			j = 0;
+			while (j < size - i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			j = 0;
+			while (j < i)
+			{
+				_putchar('#');
+				j++;
+			}
+
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
