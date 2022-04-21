dd5bcbd6499574dab31d90db64a214fd9c59d6c2
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-20 21:57:45 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts - prints every other character of a string
+ * @str: str is the string to be printed
+ *
+ * Return: Always void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
