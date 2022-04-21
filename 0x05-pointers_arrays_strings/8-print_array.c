8ef6b5d147e6a9c0a5a008502c72808b88461df9
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-21 01:13:24 -0700

    print n number of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,13 +11,15 @@
 
 void print_array(int *a, int n)
 {
+	int i;
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 		if ( i != n - 1)
 		{
-			_putchar(",");
-			_putchar(" ");
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	_putchar('\n');
