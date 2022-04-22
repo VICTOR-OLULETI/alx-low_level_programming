b576e5002dfdc79ec32c8703937dba2ba25c7a90
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-21 21:03:05 -0700

    random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void randompassword(int N)
+{
+	int i = 0;
+
+	int randomizer = 0;
+
+	srand((unsigned int)(time(NULL)));
+
+	char numbers[] = "0123456789";
+	char letter[] = "abcdefghijklmnopqrstuvwxyz";
+	char LETTER[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char SYMBOLS[] = "!@#$^&*?";
+	char password[N];
+
+	randomizer = rand() % 4;
+	for (i = 0; i < N; i++)
+	{
+		if (randomizer == 1)
+		{
+			password[i] = numbers[rand() % 10];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2)
+		{
+			password[i] = symbols[rand() % 8];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 3)
+		{
+			password[i] = LETTER[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = letter[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+	}
+}
