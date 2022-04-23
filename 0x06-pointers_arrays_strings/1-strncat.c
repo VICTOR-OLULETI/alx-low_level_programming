e2685c76e868a0f51ee0441fae589cae3e3ec96a
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-23 16:43:22 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,23 +7,25 @@
  * @n: the number of bytes of the second string to be
  * concatenated
  *
+ * Return: Always char.
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j;
+	int sum;
 
 	while(dest[i])
 	{
 		i++;
 	}
-
+	sum = i + j;
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
-		dest[i + j] = src[j];
+		dest[sum] = src[j];
 	}
-	dest[i + j + 1] = '\0';
+	dest[sum++] = '\0';
 
 	return (dest);
 }
