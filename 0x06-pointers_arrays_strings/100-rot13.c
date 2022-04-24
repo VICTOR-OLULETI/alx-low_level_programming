1e28d622040e90c68fe459df5b16d05d3d035139
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-24 14:56:21 -0700

    encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *rot13 - function that encodes a string using rot13
+ * @str: string to be encoded.
+ *
+ * Return: Always char.
+ */
+
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] = '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z'
+		    || str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 13;
+		}
+	}
+	return (str);
+}
