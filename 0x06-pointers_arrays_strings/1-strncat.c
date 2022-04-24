3bd4d83c88acde200d6530a78462cb59b2b2aa4c
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-23 17:31:30 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,9 +13,9 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	i = 0;
-	while(dest[i])
-		i++;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[i + j] = src[j];
