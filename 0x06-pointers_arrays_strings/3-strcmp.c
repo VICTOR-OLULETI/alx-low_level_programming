89601ca03b99aa83fa4285d6531617ab2ebe8d8a
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-24 07:58:57 -0700

    create a function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i, j, k, n;
-	unsigned int diff = 0;
+        int diff = 0;
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
+
 	for (j = 0; s2[j] != '\0'; j++)
 		;
+
 	if (i > j)
 		n = i;
 	else
