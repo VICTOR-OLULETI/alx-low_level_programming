ee2d0f5e7ee6755f90d37ae77e88c7d1b2cfb0be
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-25 08:07:26 -0700

    prints a sum of two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,32 @@
 #include "main.h"
 
 /**
+ * rev - reverse array
+ * @n: integers
+ * Return: 0
+ */
+
+void rev(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (n[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = n[j];
+		n[j] = n[i];
+		n[i] = temp;
+	}
+}
+
+
+/**
  * *infinite_add - function that adds two numbers.
  *
  * @n1: n1 is the first number
@@ -12,26 +38,50 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, n;
+	int i, j, n, k;
+	int overflow = 0;
 
 	for (i = 0; n1[i] = '\0'; i++)
 		;
+	i--;
+
 	for (j = 0; n2[j] = '\0'; j++)
 		;
+	j--;
+
 	if (i > j)
-	{
-		n = i;
-	}
+        	n = i;
 	else
 		n = j;
 	if (size_r >= n)
 	{
-		for (i = 0; i < size_r; i++)
+		for (k = n; k >= 0; k--)
 		{
-			r[i] = n1[i] + n2[i];
+
+  			r[k] = n1[k] + n2[k] + overflow;
+			if (r[k] > 9)
+			{
+				r[k] = r[k] - 10;
+				overflow = 1;
+			}
+			else
+			{
+				overflow = 0;
+			}
+			if (k >= size_r - 1)
+				return (0);
+
+
 		}
+		if (k == size_r)
+			return (0);
+		r[k] = '\0';
+		rev(r);
 		return (r);
 	}
 	else
-		return (0);
+	{
+        	return (0);
+	}
+
 }
