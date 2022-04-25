7413d2ded282d1157fa9227b006a4b394bbc6d91
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-25 14:41:07 -0700

    sum of integer numbers in array

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,7 +12,7 @@ void rev_string(char *n)
 	int j = 0;
 	char temp;
 
-	while (n[i] != '\0')
+	while (*(n + i) != '\0')
 	{
 		i++;
 	}
@@ -20,9 +20,9 @@ void rev_string(char *n)
 
 	for (j = 0; j < i; j++, i--)
 	{
-		temp = n[j];
-		n[j] = n[i];
-		n[i] = temp;
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
 	}
 }
 
@@ -40,9 +40,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (n1[i] != '\0')
+	while (*(n1 + i) != '\0')
 		i++;
-	while (n2[j] != '\0')
+	while (*(n2 + j) != '\0')
 		j++;
 	i--;
 	j--;
@@ -53,11 +53,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (i < 0)
 			val1 = 0;
 		else
-			val1 = n1[i] - '0';
+			val1 = *(n1 + i) - '0';
 		if (j < 0)
 			val2 = 0;
 		else
-			val2 = n2[j] - '0';
+			val2 = *(n2 + j) - '0';
 		temp_tot = val1 + val2 + overflow;
 		if (temp_tot >= 10)
 			overflow = 1;
@@ -65,14 +65,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			overflow = 0;
 		if (digits >= (size_r - 1))
 			return (0);
-		r[digits] = (temp_tot % 10) + '0';
+		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		j--;
 		i--;
 	}
 	if (digits == size_r)
 		return (0);
-	r[digits] = '\0';
+	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
 }
