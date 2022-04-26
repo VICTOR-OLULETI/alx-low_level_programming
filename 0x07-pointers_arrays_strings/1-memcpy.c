d3cb348f5e7deb9a8736377596bebcf077863644
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-26 14:51:43 -0700

    copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memcpy - function that copies memory area
+ * @dest: the char pointer
+ * @src: memory area
+ * @n: number of bytes
+ *
+ * Return: Always char.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
