0c9226045408a2bc384e48cc94234e82ab29e96e
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-26 19:54:43 -0700

    search a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,17 +9,5 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (accept[j] == s[i])
-			{
-
-				return (s + i);
-			}
-		}
-	}
+	return(0);
 }
