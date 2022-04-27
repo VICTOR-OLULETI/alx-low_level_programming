36eec0b836e1b6269439ed0138a67b20f5a5dbe3
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-26 18:08:46 -0700

    create a function that gets the length of a prefix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,17 +13,20 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j;
 	unsigned int k = 0;
 
-	for (i = 0; accept[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		for (j = 0; accept[j] > '\0'; j++)
 		{
-			if (accept[i] == s[j])
+			if (s[i] == accept[j])
 			{
 				k++;
 				break;
 			}
 		}
-
+		if (accept[j] == '\0')
+		{
+			break;
+		}
 	}
 	return (k);
 }
