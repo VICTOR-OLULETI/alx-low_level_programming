3a007b14e7f5c46dcb2df57c7575a403d2d61565
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-26 19:58:30 -0700

    search a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,10 +4,24 @@
  * *_strpbrk - function that searches a string for any of a set of bytes
  * @s: s is the character to be searched through
  * @accept: is the character used to search through
+ *
  * Return: Always char.
  */
 
 char *_strpbrk(char *s, char *accept)
 {
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				return(s + i);
+				break;
+			}
+		}
+	}
 	return(0);
 }
