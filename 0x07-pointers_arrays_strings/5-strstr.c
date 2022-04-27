cf7c15aaff57c793a73d11eeb1538b4877cc7f51
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-26 21:00:31 -0700

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,16 +12,20 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i,j;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; haystack[i] > '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '0'; j++)
+		for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
 		{
-			if (needle[i] == haystack[j])
+			if (haystack[j] != needle[j - i])
 			{
-				return (haystack + j);
+
 				break;
 			}
 		}
+		if (needle[j - i] == '\0')
+		{
+			return (haystack + i);
+		}
 	}
-	return(0);
+	return(0)
 }
