b0f68ba5a9920c655d6ae6a4c11fc94d2ec35bb3
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-28 09:18:59 -0700

    return the value of x raised to the power of y

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculates the natural square root of a number
+ * @n: n is number whose squre root is to be determined
+ * Return: returns the square root and if it doesn't exit returns -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+
+	if (n / i == i)
+	{
+		return (i);
+	}
+	else if (i >= n)
+	{
+		return (-1);
+	}
+	else
+	{
+		i++;
+		_sqrt_recursion(n)
+	}
+}
+	
