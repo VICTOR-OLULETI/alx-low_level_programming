b6e11c8cb0f4113223f7154ae444b13c4040013a
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-28 11:14:08 -0700

    determine if a string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion  - returns length of string
+ * @s: pointer to string parameter
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
  * is_palin - function that returns 1 if palindrome
  * @i: i is the integer parameter
  * @len: len is the length of the string
@@ -13,7 +28,7 @@ int is_palin(char *s, int i, int len)
 	{
 		if (s[i] == s[len])
 		{
-			is_palin(s, i + 1, len -1);
+			is_palin(s, i + 1, len - 1);
 			return (1);
 		}
 	}
