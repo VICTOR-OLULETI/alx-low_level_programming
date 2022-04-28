187713da00e14709e800c9f61496a8bbdf730323
OLULETI VICTOR <oluletiv@gmail.com>
2022-04-28 07:13:09 -0700

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints the string entered
+ * @s: s is the string entered
+ * Return: Always void.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s[0])
+	{
+		_putchar(s[0]);
+		s = s + 1;
+		_puts_recursion(s);
+	}
+	_putchar('\n');
+}
