68efcd7f93c72feb21429ad9320778038ddb6be1
OLULETI VICTOR <oluletiv@gmail.com>
2022-05-02 15:46:23 -0700

    prints the number of arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ *
+ * @argc: argc is the number of arguments
+ * @argv: argv is the array
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0, i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
