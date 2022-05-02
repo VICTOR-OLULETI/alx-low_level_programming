f951921f1aed199827102d48705f6c9644b4f2c9
OLULETI VICTOR <oluletiv@gmail.com>
2022-05-02 15:30:53 -0700

    print it name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -7,9 +8,8 @@
  * Return: Always 0.
  */
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
