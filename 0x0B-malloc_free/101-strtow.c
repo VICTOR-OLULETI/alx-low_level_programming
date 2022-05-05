dd4f06f410088728142261496b4e3324eeb36184
OLULETI VICTOR <oluletiv@gmail.com>
2022-05-05 16:22:34 -0700

    split a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,81 +1,95 @@
-#include <stdlib.h>
+#include "main.h"
+#include "stdlib.h"
+#include "stdio.h"
 
 /**
- * strtow - char
- * @str: pointer to string params
- * Return: char
+ * word_len - finds the length of a word
+ * @str: string to test
+ *
+ * Return: int
+ */
+
+int word_len(char *str)
+{
+	int i = 0, len = 0;
+
+	while (*(str + i) && *(str + i) != ' ')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * word_count - counts the number of words
+ *
+ * @str: inut
+ *
+ * Return: number of words
+ *
+ */
+
+int word_count(char *str)
+{
+	int i = 0, len = 0, count = 0;
+
+	for(i = 0; *(str + i); i++)
+		len++;
+	for (i = 0; i < len; i++)
+	{
+		if (*(str + i) != ' ')
+		{
+			count++;
+			i += word_len(str + i);
+		}
+	}
+	return (count);
+}
+
+/**
+ * strtow - splits a string into words
+ *
+ * @str: input
+ *
+ * Return: 0 - success
+ *
  */
 
 char **strtow(char *str)
 {
-	int i = 0, j = 0, k = 0;
-	int len = 0, count = 0;
-	char **f, *col;
+	int i, words, w, letters, l;
+	char **p;
 
-	if (!str || !*str)
-		return (NULL);
-
-	while (*(str + i))
+	if (str == NULL || str[0] == '\0')
 	{
-		if (*(str + i) != ' ')
-		{
-			if (*(str + i + 1) == ' ' || *(str + i + 1) == 0)
-			{
-				count += 1;
-			}
-		}
-		i++;
+		return (NULL);
 	}
-
-	if (count == 0)
+	words = word_count(str);
+	if (words == 0)
 		return (NULL);
-
-	count += 1;
-	f = malloc(sizeof(char *) * count);
-
-	if (!f)
+	p = malloc(sizeof(char *) * (words + 1));
+	if (p == NULL)
 		return (NULL);
-	i = 0;
-
-	while (*str)
+	for (i = 0; i < words; i++)
 	{
-		while (*str == ' ' && *str)
+		while (*(str + w) == ' ')
+			w++;
+		letters = word_len(str + w);
+		p[i] = malloc(sizeof(char) * (letters + 1));
+		if (p[i] == NULL)
 		{
-			str++;
-		}
-		len = 0;
-
-		while (*(str + len) != ' ' && *(str + len))
-		{
-			len += 1;
-		}
-		len += 1;
-		col = malloc(sizeof(char) * len);
-
-		if(!col)
-		{
-			for (k = j - 1; k >= 0; k--)
-			{
-				free(f[k]);
-			}
-			free(f);
+			for (; i >= 0; i--)
+				free(p[i]);
+			free(p);
 			return (NULL);
-
 		}
-		for (k = 0; k < (len - 1); k++)
+		for (i = 0; l < letters; l++)
 		{
-			*(col + k) = *(str++);
+			p[i][l] = str[w++];
 		}
-		*(col + k) = '\0';
-		*(f + j) = col;
-
-		if (j < (count - 1))
-		{
-			j++;
-		}
+		p[i][l] = '\0';
 	}
-	*(f + j) = NULL;
-	return (f);
-
-
+	p[i] = NULL;
+	return (p);
 }
