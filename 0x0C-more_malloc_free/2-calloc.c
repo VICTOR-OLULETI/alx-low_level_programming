da37215c5e0fe4fcb189300152e799045ccd13fa
OLULETI VICTOR <oluletiv@gmail.com>
2022-05-09 12:32:32 -0700

    allocate memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,9 +16,12 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
+	{
 		s[i] = b;
+	}
 	return (s);
 }
+
 /**
  * *_calloc - allocates memory for an array using malloc
  *
@@ -35,7 +38,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(sizeof(nmemb) * size);
+	p = malloc(nmemb * size);
+	
 	if (!p)
 		return (NULL);
 	_memset(p, 0, (nmemb * size));
