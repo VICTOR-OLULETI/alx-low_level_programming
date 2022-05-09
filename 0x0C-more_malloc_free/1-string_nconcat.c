d700ddde85c32228e9cc3692417953ae541266af
OLULETI VICTOR <oluletiv@gmail.com>
2022-05-09 10:01:44 -0700

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1, len2, i, j;
 
 	if (s1 == NULL)
-		s1 = ' ';
+		s1 = " ";
+
 	if (s2 == NULL)
-		s2 = ' ';
+		s2 = " ";
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
