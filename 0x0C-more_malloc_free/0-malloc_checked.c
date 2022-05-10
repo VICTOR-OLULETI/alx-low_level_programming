cf7e706bc394186172a8f12c323761355678a8f0
OLULETI VICTOR <oluletiv@gmail.com>
2022-05-09 17:29:31 -0700

    adjust message

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * malloc_checked - allocates memory using malloc
+ * *malloc_checked - allocates memory using malloc
  * @b - b is the integer variable
  *
  * Return: return a pointer or NULL to the alocated memory
