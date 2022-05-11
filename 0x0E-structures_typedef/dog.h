fe58b0f5ce7e1489ab76730575aee2acf8fcd6f9
OLULETI VICTOR <oluletiv@gmail.com>
2022-05-11 16:12:16 -0700

    create a new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *str);
+char *_strcpy(char *str1, char *str2);
 
 #endif
