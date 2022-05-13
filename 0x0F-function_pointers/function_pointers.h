c197d8ec02403dba14916bdb19fea32c7f3013e7
OLULETI VICTOR <oluletiv@gmail.com>
2022-05-13 16:22:30 -0700

    prints a name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
-#ifndef FUNCTION_pOINTER_H
+#ifndef FUNCTION_POINTER_H
 #define FUNCTION_POINTER_H
 
 #include <stdlib.h>
-
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
 #endif
