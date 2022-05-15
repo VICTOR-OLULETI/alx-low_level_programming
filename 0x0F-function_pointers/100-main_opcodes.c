106bc239ee40ec9884011b90f9da0286e1506583
OLULETI VICTOR <oluletiv@gmail.com>
2022-05-15 13:39:06 -0700

    prints the opcodes of its own main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(98);
+		return (1);
 	}
 
 	nbytes = atoi(argv[1]);
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	if (nbytes < 0)
 	{
 		printf("Error\n");
-		exit(99);
+		return (1);
 	}
 
 	for (i = 0; i < nbytes; i++)
