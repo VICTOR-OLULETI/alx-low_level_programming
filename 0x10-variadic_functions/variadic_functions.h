03164198e401b4440820847acbaf055a43ade81b
OLULETI VICTOR <oluletiv@gmail.com>
2022-05-16 19:49:47 -0700

    print anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,19 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+ * struch op - structure definition of op_t
+ * @op: option
+ * @func: function printed
+ */
+
+typedef struct op1
+{
+	char *op;
+	void (*func)(va_list);
+} op_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 #endif
