0983c0f7c1e6fe4aa8765aa4c3af158494a3f757
OLULETI VICTOR <oluletiv@gmail.com>
2022-05-26 21:10:37 -0700

    insert a new node at a given postion

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
  * Return: address of the new node or NULL if it fails
  */
 
-listint_t *insert_nodeint_at_index(listint_t **ead, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *new_node;
