8cc32471301deaa2c9af3a2294bb6a9549da32e0
OLULETI VICTOR <oluletiv@gmail.com>
2022-06-01 19:59:41 -0700

    reads and prints the letters to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it.
+ * @filename: name of file.
+ * @letters: number of letters it should read and print
+ *
+ * Return: number of letters it could read and print or 0 if failure.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t sz, sz2;
+	char *c;
+
+	if (!filename)
+		return (0);
+
+	c = (char *)malloc(letters * sizeof(char));
+	if (!c)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+
+	sz = read(fd, c, letters);
+	if (sz != letters)
+		return (0);
+	sz2 = write(STDOUT_FILENO, c, sz);
+	close(fd);
+	free(c);
+	if (sz2 != letters)
+		return (0);
+	else
+		return (sz2);
+}
