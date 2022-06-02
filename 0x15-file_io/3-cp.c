8410dd2c79913a30fd4f4a0860db45bf658b3c7d
OLULETI VICTOR <oluletiv@gmail.com>
2022-06-01 21:45:51 -0700

    copying content of one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,14 +9,14 @@
  *
  * Return: void.
  */
-void _error(int file_from, int file_to, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (file_to < 0)
+	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't wirte to %s\n", argv[2]);
 		exit(99);
@@ -31,38 +31,39 @@ void _error(int file_from, int file_to, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, error_close;
+	int file_from, file_to, err_close;
 	ssize_t nchars, sz;
 	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	_error(file_from, file_to, argv);
+	error_file(file_from, file_to, argv);
 
 	nchars = 1024;
 	while (nchars == 1024)
 	{
 		nchars = read(file_from, buf, 1024);
-		if (nchars < 0)
-			_error(-1, 0, argv);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
 		sz = write(file_to, buf, nchars);
-		if (sz < 0)
-			_error(0, -1, argv);
+		if (sz == -1)
+			error_file(0, -1, argv);
 	}
 
-	error_close = close(file_from);
-	if (error_close < 0)
+	err_close = close(file_from);
+	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	error_close = close(file_to);
-	if (error_close < 0)
+	err_close = close(file_to);
+	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
