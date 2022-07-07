b4d4b409444935ae9dc2040c6a9a0f8b19a35f7c
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-07 12:28:23 -0700

    add node to a given index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = current;
 	new_node->prev = current->prev;
-	if (curent->prev != NULL)
+	if (current->prev != NULL)
 		current->prev->next = new_node;
 	return (new_node);
 }
