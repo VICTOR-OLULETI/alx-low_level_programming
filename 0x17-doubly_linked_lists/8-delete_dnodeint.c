dcad503093471c0666c13b06dbcd495329883c5d
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-07 19:38:36 -0700

    deletes a node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -20,16 +20,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (curr == NULL)
 			return (-1);
 	}
-	if (curr->prev == NULL)
+
+	if (curr->prev != NULL)
+	{
+		curr->prev->next = curr->next;
+	}
+	else
 	{
 		*head = curr->next;
 		if (curr->next != NULL)
 			curr->next->prev = NULL;
 		free(curr);
-	}
-	else
-	{
-		curr->prev->next = curr->next;
 	}
 	return (1);
 }
