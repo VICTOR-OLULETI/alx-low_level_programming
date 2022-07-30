a95077f81cfc2c848a234c07c415e3f9549158df
OLULETI VICTOR <oluletiv@gmail.com>
2022-07-29 17:38:32 -0700

    adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,7 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	add_node = malloc(sizeof(hash_node_t));
 	if (!add_node)
 		return (false);
-	add_node->key = strdup(key) add_node->value = strdup(value);
+	add_node->key = strdup(key), add_node->value = strdup(value);
 	add_node->next = node;
 	ht->array[index] = add_node;
 
