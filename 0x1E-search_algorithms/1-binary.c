195bfea540cef2f74ce121173626ad4e16c62e9c
VICTOR-OLULETI <oluletiv@gmail.com>
2023-04-14 10:45:51 +0100

    implement binary algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,47 @@
+#include "search_algos.h"
+#include "math.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * using binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index where value is located or -1 if array is NULL.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t L, R, i;
+	int m;
+
+	L = 0;
+	R = size - 1;
+	while (L != R)
+	{
+		printf("Searching in array: ");
+		for (i = L; i <= R; i++)
+		{
+			if (i == R)
+				printf("%d\n", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+		m = floor((L + R) / 2);
+		if (array[m] < value)
+		{
+			L = m + 1;
+		}
+		else if (array[m] > value)
+		{
+			R = m - 1;
+		}
+		else
+			break;
+	}
+	if (array[m] == value)
+	{
+		return (m);
+	}
+	return (-1);
+}
+
